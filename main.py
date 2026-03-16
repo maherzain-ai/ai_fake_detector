55ce63bd1379e643971a37a23a4e2a91492d12e2
maherzain-ai <imamzain687123@gmail.com>
2026-03-16 12:17:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ import os
 if __name__ == "__main__":
     port=int(os.environ.get("PORT",10000))
     uvicorn.run("main:app",
-                host="0.0.0",port=port)
+                host="0.0.0.0",port=port)
 
     
     
